a4a5fd50fa4e52071b3dfaefb2602a4f6261d131
FeresBenAissa2 <feres.benaissa@esprit.tn>
2023-10-13 16:04:55 +0100

    updateGestionException

--- src/tn/tuniprob/gestionException/TestException.java
+++ src/tn/tuniprob/gestionException/TestException.java
@@ -3,15 +3,15 @@ package tn.tuniprob.gestionException;
 import java.io.*;
 
 public class TestException {
-    public static void main(String[] args) {
-    try{
+    public static void main(String[] args) throws FileNotFoundException,IOException {
+   // try{
         //Open the Input file
         File inputFile = new File("Sample.jpg");
         //Open the Output file
         File outputFile1 = new File("Output1.jpg");
 
         //Get file handlers in Byte Stream format
-        FileInputStream in1 = new FileInputStream(inputFile);
+        FileInputStream in1 = new FileInputStream(inputFile) ;
         FileOutputStream out1 = new FileOutputStream(outputFile1);
 
         int c1;
@@ -24,7 +24,7 @@ public class TestException {
         in1.close();
         out1.close();
 
-    } catch (FileNotFoundException f){
+  /*  } catch (FileNotFoundException f){
         System.out.println("FileNotFoundException");
     } catch (IOException Ioe){
         System.out.println("The file you have requested cannot be found.");
@@ -33,7 +33,7 @@ public class TestException {
     }
     finally {
         System.out.println("do nothing");
-    }
+    }*/
 
 
     }
---
+++ src/tn/tuniprob/gestionException/TestFinally.java
@@ -0,0 +1,22 @@
+package tn.tuniprob.gestionException;
+
+import java.io.IOException;
+
+public class TestFinally {
+    public static void main(String[] args) throws IOException {
+        try {
+            System.out.println("entrer votre code ");
+            // traiter cette exception
+            int x = System.in.read();
+        } catch (IOException e) {
+            System.out.println(e);
+        } finally {
+            System.out.println("Merci de votre visite");
+        }
+
+
+        // afficher toujours le message "Merci de votre visite"
+
+    }
+
+}
